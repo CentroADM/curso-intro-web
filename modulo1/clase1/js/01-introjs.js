f6c9d18701845d04bdf1ebbb11169f0ed4591191
furenku <furenku@gmail.com>
2018-03-03 14:04:53 -0600

    clase 2

---
+++ modulo1/clase1/js/01-introjs.js
@@ -0,0 +1,46 @@
+
+
+console.log( "escribir algo en consola" )
+
+// un comentario de una línea
+
+/*
+unhacerAlgoCuandoClick()
+comentario
+multilínea
+*/
+
+
+function hacerAlgoCuandoClick()
+{
+
+  titulo = document.getElementById("el-titulo")
+
+  posY = 500 * Math.random()
+
+  posX = 1000 * Math.random()
+
+  separacion = 15 * Math.random()
+
+  titulo.style = "color:red; letter-spacing:"+separacion+"px; top:"+posY+"px; left:"+posX+"px;"
+
+  // alert("hiciste click")
+
+}
+
+
+/*
+setTimeout(
+  function() {
+    console.log("algo sucede")
+  },
+  5000
+)
+
+setInterval(
+  function() {
+    console.log("algo sucede muchas veces")
+  },
+  1500
+)
+*/
